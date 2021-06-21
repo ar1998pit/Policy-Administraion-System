f1105e81eda9e5c5b77cbb6c9829edbd88e072ea
Arpit Srivastava <53327528+ar1998pit@users.noreply.github.com>
2021-06-21 11:15:11 +0530

    Add files via upload

---
+++ Authorization/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO `users` (email,password, firstname,lastname) values ('saravanan21@gmail.com','saravanan123', 'Saravanan', 'B');
+INSERT INTO `authorities` (role,users_id) values ('ROLE_ADMIN',1);
+INSERT INTO `users` (email,password, firstname,lastname) values ('ar1998pit@gmail.com','Arpit9876', 'Arpit', 'Srivastava');
+INSERT INTO `authorities` (role,users_id) values ('ROLE_ADMIN',2);
+INSERT INTO `users` (email,password, firstname,lastname) values ('nilanshuv@gmail.com','nilanshu12', 'Nilanshu', 'Vishesh');
+INSERT INTO `authorities` (role,users_id) values ('ROLE_ADMIN',3);
+INSERT INTO `users` (email,password, firstname,lastname) values ('sattv123@gmail.com','sattv321', 'Sattv', 'Agarwal');
+INSERT INTO `authorities` (role,users_id) values ('ROLE_USER',4);
+INSERT INTO `users` (email,password, firstname,lastname) values ('bhavyas22@gmail.com','bhavya123', 'Bhavya', 'Sharma');
+INSERT INTO `authorities` (role,users_id) values ('ROLE_USER',5);
